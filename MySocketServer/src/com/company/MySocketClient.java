ffab97251d316bd55b02106aa653f9d0eca71eb7
dumbokim <80529029+dumbokim@users.noreply.github.com>
2021-07-22 17:48:05 +0900

    Update MySocketClient.java

--- MySocketServer/src/com/company/MySocketClient.java
+++ MySocketServer/src/com/company/MySocketClient.java
@@ -10,7 +10,7 @@ public class MySocketClient {
         try {
             Socket socket = null;
             //소켓 서버에 접속
-            socket = new Socket("192.168.68.110", 1234);
+            socket = new Socket("url or ip"/* tcp url from ngrok or ip */, 8080/* tcp port from ngrok or port */);
             System.out.println("서버에 접속 성공!"); //접속 확인
 
             // 서버에서 보낸 메세지 읽는 Thread
